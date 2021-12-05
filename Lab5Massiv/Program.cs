eba17da981a54ee54b88cb38402d8dc562462854
Ирина_Колясева <irinakolyaseva95@gmail.com>
2021-12-05 14:03:42 +0300

    Добавьте файлы проекта.

---
+++ Lab5Massiv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Massiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double[] numb = new double[7];
+                double SA=0;
+                for (int i = 0; i < numb.Length; i++)
+                {
+                    Console.Write($"Введите элемент массива {i+1}: ");
+                    numb[i]=double.Parse(Console.ReadLine());
+                    SA+=numb[i]/7;
+                }
+                Console.WriteLine(SA);
+            }
+            Console.ReadKey();
+        }
+     
+    }
+}
---
+++ zadacha 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.WriteLine("Y: " + i + "X: " + j);
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    Console.Write(array[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
